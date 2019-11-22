8a519af1a373578e612033365160132923c35349
Ian Huang <ianhuang46@gmail.com>
2019-11-22 12:37:29 -0500

    C1 -> colony 1

--- Game Of Life/Views/MainView.swift
+++ Game Of Life/Views/MainView.swift
@@ -2,31 +2,31 @@ import SwiftUI
 
 struct MainView : View {
     @State var colonyData = [
-        Colony("C1", 60,
+        Colony("Colony 1", 60,
             [Coordinate(5,5),
             Coordinate(5,6),
             Coordinate(5,7),
             Coordinate(6,6)
         ]),
-        Colony("C2", 60,
+        Colony("Colony 2", 60,
             [Coordinate(15,15),
             Coordinate(15,16),
             Coordinate(15,17),
             Coordinate(16,16)
         ]),
-        Colony("C3", 60,
+        Colony("Colony 3", 60,
             [Coordinate(25,25),
             Coordinate(25,26),
             Coordinate(25,27),
             Coordinate(26,26)
         ]),
-        Colony("C4", 60,
+        Colony("Colony 4", 60,
             [Coordinate(35,35),
             Coordinate(35,36),
             Coordinate(35,37),
             Coordinate(36,36)
         ]),
-        Colony("C5", 60,
+        Colony("Colony 5", 60,
             [Coordinate(45,45),
             Coordinate(45,46),
             Coordinate(45,47),
