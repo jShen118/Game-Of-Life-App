e207cd3d8e2f4f5ef6f4315ee1b3c4302d65b975
Ian Huang <ianhuang46@gmail.com>
2019-11-29 14:35:43 -0500

    Pulled the fattest prank on SwiftUI by resorting to CoreGraphics to render the colony grid

--- Game Of Life/Views/ColonyView.swift
+++ Game Of Life/Views/ColonyView.swift
@@ -2,6 +2,20 @@ import SwiftUI
 import Foundation
 import Combine
 
+public extension UIImage {
+  public convenience init?(color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) {
+    let rect = CGRect(origin: .zero, size: size)
+    UIGraphicsBeginImageContextWithOptions(rect.size, false, 0.0)
+    color.setFill()
+    UIRectFill(rect)
+    let image = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+
+    guard let cgImage = image?.cgImage else { return nil }
+    self.init(cgImage: cgImage)
+  }
+}
+
 struct ColonyView: View {
     @Binding var colony: Colony
     @State var evolveTime = 1.0
@@ -11,24 +25,51 @@ struct ColonyView: View {
     @State var isEvolving = false
     @State var wrap = false
     
-    var gridView: some View {
-        VStack(spacing: 0) {
-            ForEach(0..<60) { row in
-                HStack(spacing: 0) {
-                    ForEach(0..<60) { col in
-                        Rectangle().fill(self.colony.isCellAlive(Coordinate(row, col)) ? Color.green : Color.red)
-                            .frame(width: 10, height: 10)
-                            .padding(1)
-                            .onTapGesture {self.colony.toggleLife(Coordinate(row, col))}
-                    }
+    func onDragAt(point: CGPoint) {
+        let row = Int(point.y/10)
+        let col = Int(point.x/10)
+        print(row,col)
+        self.colony.setCellAlive(Coordinate(row, col))
+    }
+    
+    func renderGrid()->UIImage {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 600, height: 600))
+        let img = renderer.image { ctx in
+            ctx.cgContext.setFillColor(UIColor.blue.cgColor)
+
+            let rectangle = CGRect(x: 0, y: 0, width: 600, height: 600)
+            ctx.cgContext.addRect(rectangle)
+            
+            for row in 0..<60 {
+                for col in 0..<60 {
+                    let rectangle = CGRect(x: col*10, y: row*10, width: 10, height: 10)
+                    ctx.cgContext.addRect(rectangle)
+                    let color = colony.isCellAlive(Coordinate(row, col)) ? UIColor.green : UIColor.red
+                    
+                    ctx.cgContext.setFillColor(color.cgColor)
+                    ctx.cgContext.addRect(rectangle)
+                    ctx.cgContext.drawPath(using: .fillStroke)
                 }
             }
-        }.onReceive(evolutionTimer) {_ in
+            ctx.cgContext.drawPath(using: .fillStroke)
+        }
+        return img
+    }
+    
+    var gridView: some View {
+        Image(uiImage: renderGrid()).onReceive(evolutionTimer) {_ in
             if self.isEvolving {
                 if self.wrap {self.colony.evolveWrap()}
                 else {self.colony.evolve()}
             }
         }.drawingGroup()
+        .gesture(DragGesture(minimumDistance: 0)
+            .onChanged { value in
+                self.onDragAt(point: value.location)
+        }   // 4.
+            .onEnded { value in
+            }
+        )
     }
     
     var body: some View {
