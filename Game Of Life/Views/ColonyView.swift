7acb1934ef4f56fdf18b83e889c9f47bbd01695a
Ian Huang <ianhuang46@gmail.com>
2019-11-29 14:36:06 -0500

    uh actually i didn't need that part

--- Game Of Life/Views/ColonyView.swift
+++ Game Of Life/Views/ColonyView.swift
@@ -2,20 +2,6 @@ import SwiftUI
 import Foundation
 import Combine
 
-public extension UIImage {
-  public convenience init?(color: UIColor, size: CGSize = CGSize(width: 1, height: 1)) {
-    let rect = CGRect(origin: .zero, size: size)
-    UIGraphicsBeginImageContextWithOptions(rect.size, false, 0.0)
-    color.setFill()
-    UIRectFill(rect)
-    let image = UIGraphicsGetImageFromCurrentImageContext()
-    UIGraphicsEndImageContext()
-
-    guard let cgImage = image?.cgImage else { return nil }
-    self.init(cgImage: cgImage)
-  }
-}
-
 struct ColonyView: View {
     @Binding var colony: Colony
     @State var evolveTime = 1.0
